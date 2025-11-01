affa6a9876caab2bab32ad2c1bfb5dd3c5130d36
Ian Torres <iantorres@outlook.com>
2025-11-01 05:57:37 -0300

    State can manage queues.

---
+++ tests/state.cc
@@ -0,0 +1,80 @@
+// Copyright (C) 2025 Ian Torres <iantorres@outlook.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+#include <gtest/gtest.h>
+
+#include <queue/state.hpp>
+
+TEST(state, can_create_shared_instance) {
+  const auto _state = std::make_shared<queue::state>();
+  ASSERT_NE(_state.get(), nullptr);
+}
+
+TEST(state, can_manage_queues) {
+  const auto _state = std::make_shared<queue::state>();
+  std::string _channel = "notifications";
+
+  {
+    const auto _queue = _state->add_queue(_channel);
+    ASSERT_EQ(_state->queues().size(), 1);
+    ASSERT_NE(_queue.get(), nullptr);
+    ASSERT_TRUE(_state->queue_exists(_channel));
+  }
+  ASSERT_TRUE(_state->queue_exists(_channel));
+
+  {
+    _state->remove_queue(_channel);
+    ASSERT_EQ(_state->queues().size(), 0);
+    ASSERT_FALSE(_state->queue_exists(_channel));
+  }
+
+  {
+    const auto _queue = _state->add_queue(_channel);
+    ASSERT_EQ(_state->queues().size(), 1);
+    ASSERT_NE(_queue.get(), nullptr);
+    ASSERT_TRUE(_state->queue_exists(_channel));
+    _state->remove_queue(_channel);
+    ASSERT_EQ(_state->queues().size(), 0);
+    ASSERT_FALSE(_state->queue_exists(_channel));
+  }
+
+  {
+    const std::string _on_missing_create = "ops";
+    ASSERT_FALSE(_state->queue_exists(_on_missing_create));
+    ASSERT_NE(_state->get_queue(_on_missing_create).get(), nullptr);
+    ASSERT_EQ(_state->queues().size(), 1);
+    _state->remove_queue(_on_missing_create);
+    ASSERT_EQ(_state->queues().size(), 0);
+  }
+
+  {
+    const auto _queue = _state->add_queue(_channel);
+    ASSERT_EQ(_state->queues().size(), 1);
+    ASSERT_NE(_queue.get(), nullptr);
+
+    const auto _same_queue = _state->get_queue(_channel);
+    ASSERT_EQ(_same_queue.get(), _queue.get());
+    _state->remove_queue(_channel);
+  }
+
+  {
+    const auto _queue = _state->add_queue(_channel);
+    ASSERT_EQ(_state->queues().size(), 1);
+    ASSERT_NE(_queue.get(), nullptr);
+
+    const auto _same_queue = _state->add_queue(_channel);
+    ASSERT_EQ(_state->queues().size(), 1);
+    ASSERT_EQ(_same_queue.get(), _queue.get());
+  }
+}
