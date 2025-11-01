b94c5fe6a56de923c92a3383e6ba56a431a8544a
Ian Torres <iantorres@outlook.com>
2025-11-01 06:33:19 -0300

    Sonar coverage fixed.

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,3 +5,6 @@ mkdir -p build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DENABLE_TESTS=ON
 make -j4
+ctest  --output-on-failure --verbose
+gcovr --root . --filter='src/' --sonarqube -o sonar-coverage.xml \
+      --print-summary --gcov-ignore-parse-errors=negative_hits.warn
